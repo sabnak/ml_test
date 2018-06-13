cf51f492901adb6484368f0502ee110c76a9886e
sabnak <yithiann@gmail.com>
2018-06-13 22:33:53 +0300

    Тесты

--- t.py
+++ t.py
@@ -1,8 +1,12 @@
-import random
-a = random.sample(range(1, 100), 10)
-print(a)
+def a(a):
+	i = 3
+	while True:
+		yield a ** i
+		i += 1
 
-a = map(lambda k: k[0] + k[1], enumerate(a))
 
-print(list(enumerate(a)))
-# Надо получить новый  список, в котором к каждому элементу списка A, значение которого больше значения следующего, был бы прибавлен его индекс.
+d = a(2)
+
+print(next(d))
+print(next(d))
+print(next(d))
