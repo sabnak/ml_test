d3bd3bae74311343659131ea3bfef81ff687f3f3
yithian <yithiann@gmail.com>
2018-06-30 10:11:23 +0300

    Спам

--- t.py
+++ t.py
@@ -2,24 +2,30 @@ import numpy as np
 from scipy.sparse import csr_matrix
 import sys
 
-print(sys.path)
+# print(sys.path)
+#
+#
+# X = np.array([
+# 	[0, 1, 0, 1],
+# 	[1, 0, 1, 1],
+# 	[0, 0, 0, 1],
+# 	[1, 0, 1, 0]
+# ])
+# y = np.array([0, 1, 0, 1])
+#
+# print(X[y == 0])
+#
+# t = np.random.randint(1, 100, (5, 5))
+#
+#
+# row = np.array([0, 0, 1, 2, 2, 2])
+# col = np.array([0, 2, 2, 0, 1, 2])
+# data = np.array([1, 2, 3, 4, 5, 6])
+# print(csr_matrix((data, (row, col)), shape=(3, 3)).toarray())
 
 
-X = np.array([
-	[0, 1, 0, 1],
-	[1, 0, 1, 1],
-	[0, 0, 0, 1],
-	[1, 0, 1, 0]
-])
-y = np.array([0, 1, 0, 1])
+x = np.array([[[0, 3], [1, 3], [2, 3]]])
+print(x)
 
-print(X[y == 0])
-
-t = np.random.randint(1, 100, (5, 5))
-
-
-row = np.array([0, 0, 1, 2, 2, 2])
-col = np.array([0, 2, 2, 0, 1, 2])
-data = np.array([1, 2, 3, 4, 5, 6])
-print(csr_matrix((data, (row, col)), shape=(3, 3)).toarray())
+print(np.squeeze(x))
 
--- transformer.py
+++ transformer.py
@@ -1,92 +1,14 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
-import re
-from html import unescape
 import nltk
-import urlextract
 
 # column index
 rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
 
 stemmer = nltk.PorterStemmer()
-url_extractor = urlextract.URLExtract()
-
 
 def indices_of_top_k(arr, k):
 	return np.sort(np.argpartition(np.array(arr), -k)[-k:])
-
-
-def html_to_plain_text(html):
-
-	replacement_list = [
-		(r"<head.*?>.*?</head>", ""),
-		(r"<a\s.*?>", " HYPERLINK "),
-		(r"<.*?>", ""),
-		(r"(\s*\n)+", "\n"),
-	]
-
-	for (pattern, replacement) in replacement_list:
-		html = re.sub(pattern, replacement, html, flags=re.S | re.I)
-
-	return unescape(html)
-
-
-def email_to_text(email, unify_urls=True, unify_numbers=True):
-
-	try:
-		from_ = email.get("from")
-	except IndexError:
-		from_ = None
-
-	domain = re.search("@(.+)", from_) if from_ else None
-
-	try:
-		subject = email.get("subject")
-	except IndexError:
-		subject = None
-
-	content = "{} {} {} ".format(
-		from_ if from_ else "EMPTYFROM",
-		domain if domain else "EMPTYDOMAIN",
-		subject if subject else "EMPTYSUBJECT"
-	)
-
-	is_html = False
-
-	for part in email.walk():
-		content_type = part.get_content_type()
-
-		if content_type not in ("text/plain", "text/html"):
-			continue
-
-		try:
-			content += part.get_content()
-		except:
-			content += str(part.get_payload())
-
-		if content_type == "text/html":
-			is_html = True
-
-		break
-
-	content = content.strip()
-
-	if not content:
-		return ""
-
-	if is_html:
-		content = html_to_plain_text(content)
-
-	if unify_numbers:
-		content = re.sub(r"[\d][\d.]*", " NUMBER ", content)
-
-	if unify_urls:
-		urls = list(set(url_extractor.find_urls(content)))
-		urls.sort(key=lambda url: len(url), reverse=True)
-		for url in urls:
-			content = content.replace(url, " URL ")
-
-	return str(content)
 
 
 class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
